2fa3656bd796a2169f9ec448500f8a9cc126d1ea
Dave Syer <dsyer@vmware.com>
2010-07-01 17:18:04 +0200

    SpringTest includes transaction manager and rollback test

--- modules/activiti-engine-test-cfg/src/test/java/org/activiti/test/cfg/spring/ProcessEngineFactoryBean.java
+++ modules/activiti-engine-test-cfg/src/test/java/org/activiti/test/cfg/spring/ProcessEngineFactoryBean.java
@@ -19,79 +19,94 @@ import org.activiti.DbSchemaStrategy;
 import org.activiti.ProcessEngine;
 import org.activiti.impl.cfg.ProcessEngineConfiguration;
 import org.activiti.impl.db.IdGenerator;
+import org.activiti.impl.interceptor.Command;
+import org.activiti.impl.interceptor.CommandExecutor;
+import org.activiti.impl.interceptor.CommandInterceptor;
+import org.activiti.impl.interceptor.DefaultCommandExecutor;
 import org.activiti.impl.persistence.IbatisPersistenceSessionFactory;
 import org.activiti.impl.persistence.PersistenceSessionFactory;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.util.Assert;
 
 /**
  * @author Dave Syer
  */
-public class ProcessEngineFactoryBean implements FactoryBean {
+public class ProcessEngineFactoryBean implements FactoryBean<ProcessEngine> {
 
-	private String databaseName;
-	private DataSource dataSource;
-	private PlatformTransactionManager transactionManager;
-	private DbSchemaStrategy dbSchemaStrategy;
-	private boolean jobExecutorAutoActivate;
-	private String processEngineName;
+  private String databaseName;
+  private DataSource dataSource;
+  private PlatformTransactionManager transactionManager;
+  private DbSchemaStrategy dbSchemaStrategy;
+  private boolean jobExecutorAutoActivate;
+  private String processEngineName;
 
-	public Object getObject() throws Exception {
+  public ProcessEngine getObject() throws Exception {
 
-		Assert.state(databaseName != null,
-				"A database name must be provided (e.g. 'h2')");
+    Assert.state(databaseName != null, "A database name must be provided (e.g. 'h2')");
 
-		ProcessEngineConfiguration configuration = new ProcessEngineConfiguration();
-		configuration.setDbSchemaStrategy(dbSchemaStrategy);
-		configuration.setJobExecutorAutoActivate(jobExecutorAutoActivate);
-		configuration.setProcessEngineName(processEngineName);
-		IdGenerator idGenerator = configuration.getIdGenerator();
-		PersistenceSessionFactory persistenceSessionFactory = new IbatisPersistenceSessionFactory(
-				idGenerator, databaseName, dataSource,
-				transactionManager == null);
-		configuration.setPersistenceSessionFactory(persistenceSessionFactory);
+    ProcessEngineConfiguration configuration = new ProcessEngineConfiguration();
+    configuration.setDbSchemaStrategy(dbSchemaStrategy);
+    configuration.setJobExecutorAutoActivate(jobExecutorAutoActivate);
+    configuration.setProcessEngineName(processEngineName);
+    IdGenerator idGenerator = configuration.getIdGenerator();
+    PersistenceSessionFactory persistenceSessionFactory = new IbatisPersistenceSessionFactory(idGenerator, databaseName, dataSource, transactionManager == null);
+    configuration.setPersistenceSessionFactory(persistenceSessionFactory);
 
-		if (transactionManager != null) {
-			// configuration.setTransactionContextFactory(new
-			// SpringTransactionContextFactory(transactionManager));
-		}
+    if (transactionManager != null) {
+      // FIXME: downcast
+      DefaultCommandExecutor commandExecutor = (DefaultCommandExecutor) configuration.getCommandExecutor();
+      commandExecutor.addCommandInterceptor(new CommandInterceptor() {
 
-		return configuration.buildProcessEngine();
+        public <T> T invoke(final CommandExecutor next, final Command<T> command) {
+          // TODO: Add transaction attributes
+          @SuppressWarnings("unchecked")
+          T result = (T) new TransactionTemplate(transactionManager).execute(new TransactionCallback() {
+            public Object doInTransaction(TransactionStatus status) {
+              return next.execute(command);
+            }
+          });
+          return result;
+        }
+      });
+    }
 
-	}
+    return configuration.buildProcessEngine();
 
-	public Class<?> getObjectType() {
-		return ProcessEngine.class;
-	}
+  }
+  public Class< ? > getObjectType() {
+    return ProcessEngine.class;
+  }
 
-	public boolean isSingleton() {
-		return true;
-	}
+  public boolean isSingleton() {
+    return true;
+  }
 
-	public void setDatabaseName(String databaseName) {
-		this.databaseName = databaseName;
-	}
+  public void setDatabaseName(String databaseName) {
+    this.databaseName = databaseName;
+  }
 
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
+  public void setDataSource(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
 
-	public void setDbSchemaStrategy(DbSchemaStrategy dbSchemaStrategy) {
-		this.dbSchemaStrategy = dbSchemaStrategy;
-	}
+  public void setDbSchemaStrategy(DbSchemaStrategy dbSchemaStrategy) {
+    this.dbSchemaStrategy = dbSchemaStrategy;
+  }
 
-	public void setJobExecutorAutoActivation(boolean jobExecutorAutoActivate) {
-		this.jobExecutorAutoActivate = jobExecutorAutoActivate;
-	}
+  public void setJobExecutorAutoActivation(boolean jobExecutorAutoActivate) {
+    this.jobExecutorAutoActivate = jobExecutorAutoActivate;
+  }
 
-	public void setProcessEngineName(String processEngineName) {
-		this.processEngineName = processEngineName;
-	}
+  public void setProcessEngineName(String processEngineName) {
+    this.processEngineName = processEngineName;
+  }
 
-	public void setTransactionManager(
-			PlatformTransactionManager transactionManager) {
-		this.transactionManager = transactionManager;
-	}
+  public void setTransactionManager(PlatformTransactionManager transactionManager) {
+    this.transactionManager = transactionManager;
+  }
 
 }
